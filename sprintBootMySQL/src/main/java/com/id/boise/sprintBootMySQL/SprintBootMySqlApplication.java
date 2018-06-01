6029a0f0e86fd41b42e1a15732d851d01c99adba
RAJINI CHIMARLA <rajinichimarla@RAJINIs-MacBook-Pro.local>
2018-05-31 21:40:17 -0400

    spring boot mysql

---
+++ sprintBootMySQL/src/main/java/com/id/boise/sprintBootMySQL/SprintBootMySqlApplication.java
@@ -0,0 +1,12 @@
+package com.id.boise.sprintBootMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootMySqlApplication.class, args);
+	}
+}
